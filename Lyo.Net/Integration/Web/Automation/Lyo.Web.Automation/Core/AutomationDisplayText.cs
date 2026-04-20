795913b627f9f240e24f8219a09d80d37c554fc8
Matt Wherry <matthew.j.wherry@gmail.com>
2026-04-20 18:55:24 -0400

    added missing file

---
+++ Lyo.Net/Integration/Web/Automation/Lyo.Web.Automation/Core/AutomationDisplayText.cs
@@ -0,0 +1,18 @@
+namespace Lyo.Web.Automation;
+
+/// <summary>Shared truncation for debugger-friendly <see cref="object.ToString" /> output.</summary>
+internal static class AutomationDisplayText
+{
+    internal const int DefaultMaxLength = 120;
+
+    internal static string Ellipsis(string? s, int maxChars = DefaultMaxLength)
+    {
+        if (s is null || s.Length == 0)
+            return "";
+
+        return s.Length <= maxChars ? s : s.Substring(0, maxChars) + "…";
+    }
+
+    internal static string OptionalName(string? name)
+        => string.IsNullOrWhiteSpace(name) ? "" : $" [{name}]";
+}
